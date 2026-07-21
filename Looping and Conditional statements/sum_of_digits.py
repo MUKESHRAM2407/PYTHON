dc0d4157851a48d3079d9f02de0db4992d032ef9
MUKESHRAM__2407 <ram30032007_bec28@mepcoeng.ac.in>
2026-07-21 22:08:43 +0530

    A collection of practice snippets for digit sums and list operations (currently inactive).
    This file contains a set of commented-out code snippets exploring digit-sum calculation, list sorting to find maximum values, and list manipulation using pop(). It also includes a plain-text checklist of programming topics (Armstrong numbers, primes, Fibonacci, perfect numbers, nCr, nPr) intended as a personal to-do list for future practice programs.

---
+++ Looping and Conditional statements/sum_of_digits.py
@@ -0,0 +1,39 @@
+'''n=int(input("Enter the number: "))
+temp=n
+r=s=0
+while temp>0:
+    s=temp%10
+    r=r+s
+    temp=temp//10
+
+print(r)'''
+''''list1=[]
+n1=int(input("Enter the number of entries:"))
+for i in range(n1):
+    temp=int(input("Enter the elements:"))
+    list1.append(temp)
+print(list1)
+b=sorted(list1)
+print(list(b))
+d=[]
+c=b[-1]
+d.append(c)
+print(d)
+print(tuple(d))'''
+
+'''b=sorted(list1)
+print(b)
+print(b[-2])
+
+c=b.pop()
+print(b[-1])'''
+'''
+*amstrong
+*prime number
+*email
+*fibanocci
+*perfect
+*ncr
+*npr
+'''
+    
